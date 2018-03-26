9dc4c80c9b596373243b8a12af1aacef724c7d0a
smruthi2187 <smruthi@platform9.com>
2018-03-26 14:22:10 -0700

    Just skeleton for test cases for creating fission objs in various ns.

--- test/tests/test_obj_create_in_diff_ns.sh
+++ test/tests/test_obj_create_in_diff_ns.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+#test:disabled
+
 set -euo pipefail
 
 
-
 create_func_with_src_package() {
-
-
+    func_name=$1
+    func_ns=$2
 }
 
 create_func_with_deploy() {
-
+    func_name=$1
+    func_ns=$2
+    # fission function create --name hello-newdeploy --env python --code ~/Documents/fission-artifacts/hello.py --minscale 1 --maxscale 4 --executortype newdeploy --fns ns2 --envns ns1
 
 }
 
 create_src_package() {
     pkg_name=$1
-    pkg_namespace=$2
+    pkg_ns=$2
+    zip_file=$3
 
 }
 
 create_env() {
     env_name=$1
-    env_namespace=$2
+    env_ns=$2
     runtime_image=$3
     builder_image=$4
 
@@ -41,16 +45,23 @@ main() {
 
     # 4. cleanup all envs, funcs n pkgs.
 
+
+
     # builder mgr tests
     # 1. env with builder image and runtime image in ns1, src pkg and func in ns2, route and verify specialized pod in ns1
 
     # 2. env with builder image and runtime image in default, src pkg and func in ns2,
     #    route and verify specialized pod in fission-builder
 
+
+
     # new deploy mgr tests
     # 1. env ns1, func ns2 with code, route and verify specialized pod in ns2,
 
     # 2. env default, func ns2 with code, route and verify specialized pod in fission-function
+
+
+    log "WIP"
 
 }
 
